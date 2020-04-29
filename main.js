e26be6c8295d4db821d418d1dbec09777bdc6704
soni <yorikyorik25@gmail.com>
2020-04-29 20:21:35 +0300

    HomeWork1

---
+++ main.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let money = prompt("Ваш бюджет на месяц?", ""),
+    time = prompt('Введите дату в формате YYYY-MM-DD', "");
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings:false
+};
+
+let question = prompt("Введите обязательную статью расходов в этом месяце", ""),
+    howMoney = prompt("Во сколько обойдется?", "");
+        appData.expenses[question] =  howMoney;
+
+	question = prompt("Введите обязательную статью расходов в этом месяце", ""),
+    howMoney = prompt("Во сколько обойдется?", "");
+        appData.expenses[question] =  howMoney;
+
+
+
+alert("Ваш бюджет на день: " + appData.budget / 30);
